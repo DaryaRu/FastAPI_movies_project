bb0aa8e5bf84f2f83990bf6ed2ce1a85564e3fe9
DaryaRu <fonarina57@gmail.com>
2026-05-21 17:20:43 +0300

    fix: remove trailing slash for genre detail router

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -39,7 +39,7 @@ async def genre_list(
 
 
 @router.get(
-    "/{genre_uuid}/", response_model=Genre, summary="Получить жанр по UUID"
+    "/{genre_uuid}", response_model=Genre, summary="Получить жанр по UUID"
 )
 @cache(expire=config.CACHE_EXPIRE)
 async def genre_details(
